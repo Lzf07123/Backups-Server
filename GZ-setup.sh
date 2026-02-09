28968b42bbd9144b9ffa73cb08b70fb0db765836
Lzf07123 <l18312052638@163.com>
2026-02-09 21:36:12 +0800

    update

--- GZ-setup.sh
+++ GZ-setup.sh
@@ -3,41 +3,45 @@
 NGINX_IMAGE=https://alist.hk.lizf.cn/d/Mobile%20Cloud/backups/Backups_Server/docker_images/nginx-proxy-manager.tar?sign=zObDB2I-Nd2TOjcteNqsGszxcQ2pmiykByXrMrBY3cI=:0
 HALO_IMAGE=https://alist.hk.lizf.cn/d/Mobile%20Cloud/backups/Backups_Server/docker_images/halo_2_22.tar?sign=egCqi8_k9EAidBt1uVFPs_9b8xX73evlHEusUzlX9ng=:0
 HALODB_IMAGE=https://alist.hk.lizf.cn/d/Mobile%20Cloud/backups/Backups_Server/docker_images/postgres.tar?sign=VXsSA1hTIrYoGGN1dt0eW-Nj1PIL1PvGgkCmIRuxrkw=:0
-FRP_IMAGE=https://alist.hk.lizf.cn/d/Mobile%20Cloud/backups/Backups_Server/docker_images/frp-panel.tar?sign=tsIwCXq4Eu006E8o9O4JXeY3j4dZeIvnEGh9qrTkqHY=:0
+FRPS_IMAGE=https://alist.hk.lizf.cn/d/Mobile%20Cloud/backups/Backups_Server/docker_images/frps.tar?sign=Gbdsc6xHtdaA2IQQ5rsUX1sAsIF-VX3dy7UvfWZ3-9M=:0
+DOCGUI_IMAGE=https://alist.hk.lizf.cn/d/Mobile%20Cloud/backups/Backups_Server/docker_images/portainer-ce.tar?sign=LRAOy6xZV8PaMZ3zMWT2jBfjSNevx2wz4yCPfo7nvtc=:0
 MEILI_IMAGE=https://alist.hk.lizf.cn/d/Mobile%20Cloud/backups/Backups_Server/docker_images/meili.tar?sign=ba3EdEbppaQGTu5n-QG2vyLbdiCpS3rFK3FKBZDUYFE=:0
-KANBOARD_IMAGE=https://alist.hk.lizf.cn/d/Mobile%20Cloud/backups/Backups_Server/docker_images/kanboard.tar?sign=tPJPKxsuA1i0AGw3m63cX_tjoALhBebYa-cSYMRXs7U=:0
-RUN_ALL_FILE=https://alist.hk.lizf.cn/d/Mobile%20Cloud/backups/Backups_Server/GuangZhou/docker/all/all-compose.yml?sign=_kstMc2W91XCqY1jTG9TNQjFFaeLwee1Dynn2ZCMxrw=:0
-RUN_HALO_2_FILE=https://alist.hk.lizf.cn/d/Mobile%20Cloud/backups/Backups_Server/GuangZhou/docker/halo-2/halo-2-compose.yml?sign=VV0gYgTPKxAuI6DEmk35xgT8k5y4SLjteFy1MsvIG8c=:0
-RUN_KANBOARD_FILE=https://alist.hk.lizf.cn/d/Mobile%20Cloud/backups/Backups_Server/GuangZhou/docker/kanboard/kanboard-compose.yml?sign=BxDwcZU2ZACo0sLsMx_g_J1yUxVVAoKzQKiH-9cnueY=:0
-#HK-setup
+RUN_ALL_FILE=https://alist.hk.lizf.cn/d/Mobile%20Cloud/backups/Backups_Server/GuangZhou/docker/all-compose.yml?sign=rcts4TmseGmgX2y_RFyeEGyI_L1xyLsUzd0iz1rDiTA=:0
+RUN_HALO_2_FILE=https://alist.hk.lizf.cn/d/Mobile%20Cloud/backups/Backups_Server/GuangZhou/docker/halo-2-compose.yml?sign=Q9dhA6qx41YVa50YlzPo-vvN1Uwd_oUlIt_t5M_kMxg=:0
+RUN_DOCGUI_FILE=https://alist.hk.lizf.cn/d/Mobile%20Cloud/backups/Backups_Server/GuangZhou/docker/docgui-compose.yml?sign=lkcVTk_v9cgjzGX3vddFKz45C3Ytq29yUDrt69RuvUw=:0
+#GZ-setup
 apt-get update
 apt-get upgrade -y
-apt-get install unzip docker.io docker-compose docker-compose-v2 -y
+apt-get install wget docker.io docker-compose-v2 -y
 cd /mnt
-mkdir image && mkdir data
+mkdir image && mkdir docker
 cd image
-wget -L -O nginx.tar ${NGINX_IMAGE}
-wget -L -O halo.tar ${HALO_IMAGE}
-wget -L -O halodb.tar ${HALODB_IMAGE}
-wget -L -O frp.tar ${FRP_IMAGE}
-wget -L -O meili.tar ${MEILI_IMAGE}
-wget -L -O kanboard.tar ${KANBOARD_IMAGE}
+echo -e "\e[32m============开始拉取镜像============\e[0m"
+wget -c -L --progress=bar --show-progress -O nginx.tar ${NGINX_IMAGE} 1>/dev/null
+wget -c -L --progress=bar --show-progress -O halo.tar ${HALO_IMAGE} 1>/dev/null
+wget -c -L --progress=bar --show-progress -O halodb.tar ${HALODB_IMAGE} 1>/dev/null
+wget -c -L --progress=bar --show-progress -O frps.tar ${FRPS_IMAGE} 1>/dev/null
+wget -c -L --progress=bar --show-progress -O meili.tar ${MEILI_IMAGE} 1>/dev/null
+wget -c -L --progress=bar --show-progress -O docgui.tar ${DOCGUI_IMAGE} 1>/dev/null
 docker load -i nginx.tar
 docker load -i halo.tar
 docker load -i halodb.tar
-docker load -i frp.tar
+docker load -i frps.tar
 docker load -i meili.tar
-docker load -i kanboard.tar
-cd ../data
-mkdir halo_2 && mkdir kanboard
-wget -L -O compose.yml ${RUN_ALL_FILE}
+docker load -i docgui.tar
+echo -e "\e[32m============镜像拉取完成============\e[0m"
+cd ../docker
+mkdir halo_2 && mkdir dockergui && mkdir all
+cd all
+echo -e "\e[32m============启动容器============\e[0m"
+wget -c -L --progress=bar --show-progress -O compose.yml ${RUN_ALL_FILE} 1>/dev/null
 docker compose up -d
-cd halo_2
-wget -L -O compose.yml ${RUN_HALO_2_FILE}
+cd ../halo_2
+wget -c -L --progress=bar --show-progress -O compose.yml ${RUN_HALO_2_FILE} 1>/dev/null
 docker compose up -d
-cd ../kanboard
-wget -L -O compose.yml ${RUN_KANBOARD_FILE}
+cd ../dockergui
+wget -c -L --progress=bar --show-progress -O compose.yml ${RUN_DOCGUI_FILE} 1>/dev/null
 docker compose up -d
 rm -r /mnt/image
 docker ps
-echo '部署完成'
+read -p "$(echo -e '\e[32m============部署完成============\e[0m')"
--- HK-setup.sh
+++ HK-setup.sh
@@ -4,30 +4,36 @@ NGINX_IMAGE=https://alist.hk.lizf.cn/d/Mobile%20Cloud/backups/Backups_Server/doc
 ALIST_IMAGE=https://alist.hk.lizf.cn/d/Mobile%20Cloud/backups/Backups_Server/docker_images/alist_2.tar?sign=eCDhBfo33oTMIObvRirvlz1aH19wehTCCrt_a_KfPA8=:0
 MOMENTS_IMAGE=https://alist.hk.lizf.cn/d/Mobile%20Cloud/backups/Backups_Server/docker_images/moments.tar?sign=MNEn3ZiZq7P9SBukUkc4IbUI96xrRxZkrqADoi2awi0=:0
 XPANEL_IMAGE=https://alist.hk.lizf.cn/d/Mobile%20Cloud/backups/Backups_Server/docker_images/xui.tar?sign=XHSa1v6jrFKAuLFIAzdMtMTcpZsuH_QIEeAOxFG4cl0=:0
-FRP_IMAGE=https://alist.hk.lizf.cn/d/Mobile%20Cloud/backups/Backups_Server/docker_images/frp-panel.tar?sign=tsIwCXq4Eu006E8o9O4JXeY3j4dZeIvnEGh9qrTkqHY=:0
+AGENT_IMAGE=https://alist.hk.lizf.cn/d/Mobile%20Cloud/backups/Backups_Server/docker_images/agent.tar?sign=s1J8OGbvazjlYw6bOtpipq6ckV3654V1IyuI5JeeMxs=:0
+FRP_IMAGE=https://alist.hk.lizf.cn/d/Mobile%20Cloud/backups/Backups_Server/docker_images/frps.tar?sign=Gbdsc6xHtdaA2IQQ5rsUX1sAsIF-VX3dy7UvfWZ3-9M=:0
 RUN_FILE=https://alist.hk.lizf.cn/d/Mobile%20Cloud/backups/Backups_Server/HongKong/docker/compose.yml?sign=7ks73mYH_-sm-fuGlCJj86_okaJqd6iUof2TF3mv56Y=:0
 ENV_FILE=https://alist.hk.lizf.cn/d/Mobile%20Cloud/backups/Backups_Server/HongKong/docker/.env?sign=uFTQyOyatFr19w7RMKjdcupOe7pxb8Bw-PqIagM5iiA=:0
 #HK-setup
 apt-get update
 apt-get upgrade -y
-apt-get install unzip docker.io docker-compose docker-compose-v2 -y
+apt-get install wget unzip docker.io docker-compose-v2 -y
 cd /mnt
-mkdir image && mkdir data
+mkdir image && mkdir docker
+echo -e "\e[32m============开始拉取镜像============"
 cd image
 wget -L -O nginx.tar ${NGINX_IMAGE}
 wget -L -O alist.tar ${ALIST_IMAGE}
 wget -L -O moments.tar ${MOMENTS_IMAGE}
 wget -L -O xpanel.tar ${XPANEL_IMAGE}
 wget -L -O frp.tar ${FRP_IMAGE}
+wget -L -O agent.tar ${AGENT_IMAGE}
 docker load -i nginx.tar
 docker load -i alist.tar
 docker load -i moments.tar
 docker load -i xpanel.tar
 docker load -i frp.tar
-cd ../data
+docker load -i agent.tar
+echo -e "============镜像拉取完成============\e[0m"
+cd ../docker
+echo -e "\e[32m============启动容器============\e[0m"
 wget -L -O compose.yml ${RUN_FILE}
 wget -L -O .env ${ENV_FILE}
 docker compose up -d
 rm -r /mnt/image
 docker ps
-echo '部署完成'
+read -p "$(echo -e '\e[32m============部署完成============\e[0m')"
