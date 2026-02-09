42c9116bae6ddb56502f4d331488b7bdda673979
Lzf07123 <l18312052638@163,com>
2026-02-10 03:13:44 +0800

    update-v1.0.1

--- pg-Agent/Agent.sh
+++ pg-Agent/Agent.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# shellcheck disable=SC2125
+AGENT_IMAGE=https://alist.hk.lizf.cn/d/Mobile%20Cloud/backups/Backups_Server/docker_images/agent.tar?sign=s1J8OGbvazjlYw6bOtpipq6ckV3654V1IyuI5JeeMxs=:0
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install sudo wget -y
@@ -7,6 +9,8 @@ sudo apt-get install docker-compose-v2 -y
 cd /mnt || exit
 sudo mkdir Agent
 cd Agent || exit
+wget -L -O agent.tar "${AGENT_IMAGE}"
+docker load -i agent.tar
 cat > "compose.yml" << EOF
 services:
   agent:
@@ -19,6 +23,8 @@ services:
     network_mode: host
 EOF
 docker compose up -d
+rm agent.tar
+docker ps
 # shellcheck disable=SC2162
 echo -e "\e[32m连接地址=IP/域名:9001\e[0m"
 read -p "$(echo -e '\e[32m==========Agent安裝完成==========\e[0m')"
